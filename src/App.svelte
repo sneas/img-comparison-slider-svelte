5bab4adcbae213070c174638d987cc5ca9073f30
Dima Snisarenko <snisarenkodima@gmail.com>
2022-07-22 18:05:23 +0200

    feat: img-comparison-slider example

---
+++ src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<img-comparison-slider direction="vertical" id="piu1">
+		<!-- eslint-disable -->
+		<img
+				slot="first"
+				style="width: 100%"
+				src="https://img-comparison-slider.sneas.io/demo/images/before.webp"
+		/>
+		<img
+				slot="second"
+				style="width: 100%"
+				src="https://img-comparison-slider.sneas.io/demo/images/after.webp"
+		/>
+		<!-- eslint-enable -->
+	</img-comparison-slider>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
